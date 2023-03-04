94dc6d2ccf5f9ab2b84afec33d24f9ef8f5e1480
Riccardo Mariotti <riccardo.mariotti98@gmail.com>
2023-03-04 23:39:42 +0100

    Added searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Header from "./components/header/Header";
 import Searchbar from "./components/searchbar/Searchbar";
 import WordDefinition from "./components/word-definition/WordDefinition";
 import styles from "./App.module.scss";
 
 export default function App() {
-	const [word, setWord] = useState<string>("keyboard");
+	const [word, setWord] = useState<string>("");
+	const searchWord = useCallback(() => {
+		console.log(word);
+	}, [word]);
 
 	return (
 		<div className={styles.app}>
 			<Header />
-			<Searchbar />
+			<Searchbar word={word} setWord={setWord} placeholder="Search a word..." search={searchWord} />
 			<WordDefinition />
 		</div>
 	);
--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -1,4 +1,42 @@
+import { Dispatch, FormEvent, MouseEvent, SetStateAction, useRef } from "react";
+import { Search } from "react-bootstrap-icons";
+import styles from "./Searchbar.module.scss";
 
-export default function Searchbar() {
-	return <span>Searchbar</span>;
+interface SearchbarProps {
+	placeholder?: string;
+	word: string;
+	setWord: Dispatch<SetStateAction<string>>;
+	search: Function;
+}
+
+export default function Searchbar(props: SearchbarProps) {
+	const { placeholder, word, setWord, search } = props;
+	const inputRef = useRef<HTMLInputElement>(null);
+	const formRef = useRef(null);
+
+	function handleClick(e: MouseEvent) {
+		if (e.target === formRef.current) inputRef.current?.focus();
+	}
+
+	function submitSearh(e: FormEvent) {
+		e.preventDefault();
+		if (word) {
+			search();
+            inputRef.current?.blur();
+		}
+	}
+
+	return (
+		<form ref={formRef} className={styles.searchbar} onClick={handleClick} onSubmit={submitSearh}>
+			<input
+				ref={inputRef}
+				type="text"
+				value={word}
+				onChange={(e) => setWord(e.target.value)}
+				placeholder={placeholder}
+				className={styles["search-input"]}
+			/>
+			<Search className={styles["search-icon"]} onClick={submitSearh} />
+		</form>
+	);
 }
